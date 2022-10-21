0c712fe685fc13b14e0db9c12ea5c25f93d6b40a
charryedison <charry.edison@gmail.com>
2022-10-20 21:33:06 -0500

    10 Modificacion

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -102,6 +102,7 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  return Math.max(...numeros);
 }
 
 
@@ -109,7 +110,14 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length < 1) return 0;
+  var x = 1;
+  for (var i = 0; i < arguments.length; i++){
+    x = x * arguments[i];
+  }
+  return x;
 }
+
 
 
 function cuentoElementos(arreglo){
